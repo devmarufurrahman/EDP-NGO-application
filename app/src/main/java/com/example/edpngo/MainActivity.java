420314d066dd88a6fb04546d4c09c4be294a3289
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-10-06 22:54:13 +0600

    appbar and navigation drawer add

--- app/src/main/java/com/example/edpngo/MainActivity.java
+++ app/src/main/java/com/example/edpngo/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.edpngo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    ImageView imageMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Navagation Drawar------------------------------
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_View);
+        imageMenu = findViewById(R.id.imageMenu);
+
+
+        toggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Drawar click event
+        // Drawer item Click event ------
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if (id == R.id.navProfile){
+                    // profile menu
+
+                    Toast.makeText(MainActivity.this, "profile menu", Toast.LENGTH_SHORT).show();
+                } else if(id == R.id.navVolunteer){
+                    // volunteer menu
+                    Toast.makeText(MainActivity.this, "volunteer menu", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.navAbout){
+                    // about menu
+                    Toast.makeText(MainActivity.this, "about menu", Toast.LENGTH_SHORT).show();
+                } else if(id == R.id.navContact){
+                    // contact menu
+                    Toast.makeText(MainActivity.this, "contact menu", Toast.LENGTH_SHORT).show();
+                } else {
+                    // logout
+                    Toast.makeText(MainActivity.this, "logout menu", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+        //------------------------------
+
+        // ------------------------
+        // App Bar Click Event
+        imageMenu = findViewById(R.id.imageMenu);
+
+        imageMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Code Here
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+
+        // ------------------------
     }
 }
