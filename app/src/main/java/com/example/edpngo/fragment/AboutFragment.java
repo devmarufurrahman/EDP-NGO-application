ae6b323e12896c11f9199edcb947ba0cc9d1ce3d
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-10-14 13:31:08 +0600

    about design modify and director click event add

--- app/src/main/java/com/example/edpngo/fragment/AboutFragment.java
+++ app/src/main/java/com/example/edpngo/fragment/AboutFragment.java
@@ -2,6 +2,7 @@ package com.example.edpngo.fragment;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ public class AboutFragment extends Fragment {
 
     ImageSlider missionSlider, visionSlider;
     ArrayList<SlideModel> imgMission = new ArrayList<>();
+    CardView directorView1, directorView2, directorView3;
 
 
     public AboutFragment() {
@@ -32,16 +34,39 @@ public class AboutFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-
         missionSlider = view.findViewById(R.id.missionSlider);
         visionSlider = view.findViewById(R.id.visionSlider);
+        directorView1 = view.findViewById(R.id.directorDetails1);
+        directorView2 = view.findViewById(R.id.directorDetails2);
+        directorView3 = view.findViewById(R.id.directorDetails3);
 
+
+//        card view director activity
+        directorView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        directorView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        directorView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+//        slider image
 
         imgMission.add(new SlideModel(R.drawable.mission1,ScaleTypes.CENTER_CROP));
         imgMission.add(new SlideModel(R.drawable.mission2,ScaleTypes.CENTER_CROP));
         imgMission.add(new SlideModel(R.drawable.mission3,ScaleTypes.CENTER_CROP));
-
-
 
         missionSlider.setImageList(imgMission);
         visionSlider.setImageList(imgMission);
