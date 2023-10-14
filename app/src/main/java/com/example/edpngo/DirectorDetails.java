f679fd7b2dfd475902c5bab61de5524817807592
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-10-14 13:36:23 +0600

    director appbar  add

--- app/src/main/java/com/example/edpngo/DirectorDetails.java
+++ app/src/main/java/com/example/edpngo/DirectorDetails.java
@@ -1,6 +1,7 @@
 package com.example.edpngo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class DirectorDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_director_details);
+        Toolbar toolbar = findViewById(R.id.directorToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Director");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
