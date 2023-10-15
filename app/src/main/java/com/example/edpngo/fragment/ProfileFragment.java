b9ed4587f86a73b06026e65da7ffd9c59ecfdcf8
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-10-15 12:58:05 +0600

    volunteer profile design dynamic

--- app/src/main/java/com/example/edpngo/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/edpngo/fragment/ProfileFragment.java
@@ -2,16 +2,20 @@ package com.example.edpngo.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.edpngo.R;
 
 public class ProfileFragment extends Fragment {
 
+    TextView profileName, profilePhone, profileMail, donationTitle;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -20,6 +24,27 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profileName = view.findViewById(R.id.profileName);
+        profilePhone = view.findViewById(R.id.profilePhone);
+        profileMail = view.findViewById(R.id.profileMail);
+        donationTitle = view.findViewById(R.id.donationTitle);
+        getParentFragmentManager().setFragmentResultListener("dataBundle", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String name = result.getString("name");
+                String mail = result.getString("mail");
+                String phone = result.getString("phone");
+                String goalsTitle = result.getString("goalsTitle");
+
+                profileName.setText(name);
+                profileMail.setText(mail);
+                profilePhone.setText(phone);
+                donationTitle.setText(goalsTitle);
+            }
+        });
+
+
+        return view;
     }
 }
