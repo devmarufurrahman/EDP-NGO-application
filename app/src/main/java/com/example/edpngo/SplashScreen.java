76591660d46e0e2b5de5fbb777eb09484834146c
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-10-19 14:19:31 +0600

    lottie add and walk though 1 add

--- app/src/main/java/com/example/edpngo/SplashScreen.java
+++ app/src/main/java/com/example/edpngo/SplashScreen.java
@@ -13,7 +13,7 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Intent iHome = new Intent(SplashScreen.this,MainActivity.class);
+        Intent iHome = new Intent(SplashScreen.this,Walkthough1.class);
 
         // splash screen delay
         new Handler().postDelayed(new Runnable() {
---
+++ app/src/main/java/com/example/edpngo/Walkthough1.java
@@ -0,0 +1,33 @@
+package com.example.edpngo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Walkthough1 extends AppCompatActivity {
+
+    TextView skipWalk;
+    ImageView nextWalk1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walkthough1);
+        skipWalk = findViewById(R.id.skipWalkThough);
+        nextWalk1 = findViewById(R.id.nextWalkThough1);
+
+
+        // skip walk though
+        skipWalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Walkthough1.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
